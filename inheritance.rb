fb7b4cac38d6962d2482f637256a5e9f5767bdf4
alexanderspace100 <alexanderspace100@gmail.com>
2016-09-14 12:29:06 +0300

    ex5_inheritance

--- inheritance.rb
+++ inheritance.rb
@@ -5,54 +5,49 @@ module Sizeble
 end
 
 class Vehicle
+  attr_accessor :color
+  attr_reader :model, :year
   @@number_of_vehicles = 0
 
-  def self.number_of_vehicles
-    puts "This program has created #{@@number_of_vehicles} vehicles"
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+    @@number_of_vehicles += 1
   end
 
-  def initialize
-    @@number_of_vehicles += 1
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    puts "Speed 0"
   end
 
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
   end
+
+  def spray_paint(color)
+    self.color = color
+    puts "You new #{color} paint job looks great!"
+  end
 end
 
 class MyCar < Vehicle
   NUMBER_OF_DOORS = 4
-  attr_accessor :color
-  attr_reader :year
-
-	def initialize(year, color, model)
-		@year = year
-		@color = color
-		@model = model
-		@current_speed = 0
-	end
-
-	def speed_up(speed)
-    	@current_speed += speed
-  	end
-
-  	def brake(speed)
-    	@current_sp
-      eed -= speed
-  	end
-
-  def current_speed
-    puts "You speed is #{@current_speed} mph."
-  end
-
-  def shut_down
-    @current_speed = 0
-  end
-
-  def spray_paint(color)
-    self.color = color
-  end
-
+ 
   def to_s
     "My car is a #{color}, #{year}, #{@model}!"
   end
@@ -64,6 +59,6 @@ class MyTruck < Vehicle
   NUMBER_OF_DOORS = 2
 end
 
-puts MyCar.ancestors
-puts MyTruck.ancestors
-puts Vehicle.ancestors
+# puts MyCar.ancestors
+# puts MyTruck.ancestors
+# puts Vehicle.ancestors
